a632389fe85c9c629761b13e4c35cd219ab23b39
adriano <adriano.x1@hotmail.com>
2019-10-18 21:06:06 -0300

    Criacao do aplicativo

---
+++ WebApplication1/Controllers/ManageController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using WebApplication1.Models.AccountViewModel;
+using WebApplication1.Models.ManagerViewModel;
+
+namespace WebApplication1.Controllers
+{
+    
+
+    public class ManageController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public ManageController(UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [TempData]
+        public  string StatusMessage { get; set; }
+
+        [HttpGet]
+        public async  Task<IActionResult> Index()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null)
+            {
+                throw new ApplicationException($"Não foi possivel carregar o usuário com o ID '{_userManager.GetUserIdAsync(User)}'");
+            }
+
+            var model = new IndexViewModel
+            {
+                Username = user.UserName,
+                Email = user.Email,
+                PhoneNumber = user.PhoneNumber,
+                IsEmailConfirmed = user.EmailConfirmed,
+                StatusMessage = StatusMessage,
+            };
+
+            return View(model);
+        }
+    }
+
+}
---
+++ WebApplication1/Data/ApplicationDataContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDataContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
+    {
+
+        public ApplicationDataContext(DbContextOptions<ApplicationDataContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
